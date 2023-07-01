f48595fadca0b4ac78cf144c5cc3ab83adc067fd
Sergey <sergeypalii@gmail.com>
2023-07-01 13:04:49 +0500

    Create void

--- Final_Task/Program.cs
+++ Final_Task/Program.cs
@@ -5,3 +5,15 @@
 
 string[] array1 = new string [5] {"Mercury", "Mars", "Pluto", "fly", "cat"};
 string[] array2 = new string[array1.Length];
+void SecondArray (string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
