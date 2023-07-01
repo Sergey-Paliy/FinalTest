507410c7f62523bca32e25b2888e0555e64b0b3b
Sergey <sergeypalii@gmail.com>
2023-07-01 13:05:28 +0500

    Create voidPrint

--- Final_Task/Program.cs
+++ Final_Task/Program.cs
@@ -17,3 +17,11 @@ void SecondArray (string[] array1, string[] array2)
         }
     }
 }
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
